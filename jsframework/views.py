0c9e3c671c50c9317441deaac79e3c384361f246
Ershadul Hoque <ershadulhoque@gmail.com>
2017-09-07 12:19:52 +0600

    Add models and views

--- jsframework/views.py
+++ jsframework/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from jsframework.models import Todo
+from serializers import TodoSerializer
+
+# Todos routes automatically generated
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
 
 def index(request):
     return render(request, 'jsframework/base.html')
